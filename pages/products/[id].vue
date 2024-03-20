5e43bfca4e976d65b303219f7549546daefbcf22
shad mbwagha <shadracksylvestar@gmail.com>
2024-03-20 05:55:27 +0300

    tutorial

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>the product {{ $route.params.id }}</h3>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque minima repudiandae aut veritatis blanditiis. Ea beatae nihil tempora eius quos dolore consequatur aspernatur saepe. Exercitationem laudantium aperiam maiores vel eveniet!</p>
+</template>
+
+<script>
+    
+
+
+</script>
+
+<style>
+
+</style>
